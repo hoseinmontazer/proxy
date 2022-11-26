70e0caad94b7e9d3153b04e757446dd4dabeb864
h.montazer <h.montazer@uidco.ir>
2022-11-26 14:12:20 +0330

    some improvment

--- json2vmess.py
+++ json2vmess.py
@@ -1,133 +1,192 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import sys
+import re
 import json
+import base64
 import argparse
 import urllib.request
-import warnings
-import os
 
-V2RAYN_TEMPLATE = {
-    'v': '2',
-    'ps': '',
-    'add': '',
-    'port': '',
-    'id': '',
-    'aid': '0',
-    'net': 'tcp',
-    'type': 'none',
-    'host': '',
-    'path': '/',
-    'tls': 'none'
-}
-# Follow v2rayN format: https://github.com/2dust/v2rayN/wiki/%E5%88%86%E4%BA%AB%E9%93%BE%E6%8E%A5%E6%A0%BC%E5%BC%8F%E8%AF%B4%E6%98%8E(ver-2)
+class UnknowProtocolException(Exception):
+    pass
 
+def get_host_ip():
+    print("trying to get host ip address ...")
+    req = urllib.request.Request(url="https://www.cloudflare.com/cdn-cgi/trace")
+    with urllib.request.urlopen(req, timeout=5) as response:
+        body = response.read().decode()
+        for line in body.split("\n"):
+            if line.startswith("ip="):
+                _, _ip = line.split("=", maxsplit=2)
+                if _ip != "":
+                    print("using host ipaddress: {}, if not intended, use --addr option to specify".format(_ip))
+                    return _ip
+    return ""
 
-def generate_vmess_config(ARGS, V2RAYN_TEMPLATE):
-    with open(ARGS.config, 'r') as f:
-        config = json.load(f)
-    config = config['inbounds']
+def parse_inbounds(jsonobj):
+    vmesses = []
+    if "inbounds" in jsonobj:
+        for ib in jsonobj["inbounds"]:
+            if ib["protocol"] == "vmess":
+                try:
+                    vmesses += inbound2vmess(ib)
+                except UnknowProtocolException:
+                    pass
 
-    configOut = []
-    for iconfig in config:
-        if 'tag' not in iconfig.keys():
-            isVmess = True
-        elif iconfig['tag'] == 'vmess':
-            isVmess = True
+    for v in vmesses:
+        link = "vmess://" + base64.b64encode(json.dumps(v, sort_keys=True).encode('utf-8')).decode()
+        if option.debug:
+            msg = lambda x: "{ps} / {net} / {add}:{port} / net:{net}/aid:{aid}/host:{host}/path:{path}/tls:{tls}/type:{type}".format(**x)
+            print(msg(v))
+        print(link)
+        print()
 
-        if isVmess:
-            numClient = len(iconfig['settings']['clients'])
-            if len(ARGS.port) > 1:
-                if len(ARGS.port) != numClient:
-                    raise ValueError('cli argument <port> doesn''t match configuration file')
-                clientPort = ARGS.port
-            else:
-                clientPort = ARGS.port * numClient
+def inbound2vmess(inbound):
+    vmessobjs = []
+    _add = host_ip
+    _type = "none"
+    _host = ""
+    _path = ""
+    _tls = ""
+    _port = str(inbound["port"])
+    _net = ""
+    sset = {}
 
-            LOCAL_TEMPLATE = V2RAYN_TEMPLATE.copy()
-            LOCAL_TEMPLATE['add'] = ARGS.server
-            if 'network' in iconfig['streamSettings'].keys():
-                LOCAL_TEMPLATE['net'] = iconfig['streamSettings']['network']
-            if 'security' in iconfig['streamSettings'].keys():
-                LOCAL_TEMPLATE['tls'] = iconfig['streamSettings']['security']
-
-            if LOCAL_TEMPLATE['net'] == 'tcp':
-                LOCAL_TEMPLATE['type'] = 'none' # default: no obfuscation
-                if 'tcpSettings' in iconfig['streamSettings'].keys():
-                    warnings.warn('HTTP obfuscation is not recommended')
-                    tcpSettings = iconfig['streamSettings']['tcpSettings']
-                    if 'header' in tcpSettings:
-                        LOCAL_TEMPLATE['type'] = tcpSettings['header']['type']
-                        if LOCAL_TEMPLATE['type'] == 'http':
-                            # TCP obfuscation doesn't support path
-                            # LOCAL_TEMPLATE['path'] = tecpSettings['header']['request']['path']
-                            if 'request' in tcpSettings['header']:
-                                try:
-                                    LOCAL_TEMPLATE['host'] = ','.join(tcpSettings['header']['request']['headers']['Host'])
-                                except:
-                                    print('No host defined for http obfuscation, default values: [www.amazon.com,www.cloudflare.com] will be used')
-                                    LOCAL_TEMPLATE['host'] = 'www.cloudflare.com,www.amazon.com'
-            elif LOCAL_TEMPLATE['net'] == 'ws':  # WebSocket
-                LOCAL_TEMPLATE['type'] = 'none'
-                if 'wsSettings' in iconfig['streamSettings'].keys():
-                    wsSettings = iconfig['streamSettings']['wsSettings']
-                    if 'path' in wsSettings.keys():
-                        LOCAL_TEMPLATE['path'] = wsSettings['path']
-                    if 'headers' in wsSettings.keys():
-                        if 'Host' in wsSettings['headers'].keys():
-                            LOCAL_TEMPLATE['host'] = wsSettings['headers']['Host']
-            elif LOCAL_TEMPLATE['net'] == 'h2' or LOCAL_TEMPLATE['net'] == 'http':  # HTTP/2
-                LOCAL_TEMPLATE['net'] = 'h2'
-                LOCAL_TEMPLATE['type'] = 'none'
-                if 'httpSettings' in iconfig['streamSettings'].keys():
-                    httpSettings = iconfig['streamSettings']['httpSettings']
-                    if 'path' in httpSettings.keys():
-                        LOCAL_TEMPLATE['path'] = httpSettings['path']
-                    if 'host' in httpSettings.keys():
-                        LOCAL_TEMPLATE['host'] = ','.join(httpSettings['host'])
-                if LOCAL_TEMPLATE['tls'] == 'none':
-                    raise ValueError('HTTP/2 is not configured correctly. TLS is not enabled.')
-            elif LOCAL_TEMPLATE['net'] == 'mkcp' or LOCAL_TEMPLATE['net'] == 'kcp': # mkcp
-                LOCAL_TEMPLATE['net'] = 'kcp'
-                if 'kcpSettings' in iconfig['streamSettings'].keys():
-                    kcpSettings = iconfig['streamSettings']['kcpSettings']
-                    if 'header' in kcpSettings.keys():
-                        if 'type' in kcpSettings['header']:
-                            LOCAL_TEMPLATE['type'] = kcpSettings['header']['type']
-            else:
-                raise ValueError('wrong value for TransportObject: %s' % (LOCAL_TEMPLATE['net']))
-
-            vmessConfig = [LOCAL_TEMPLATE.copy() for i in range(numClient)]
-            for i, (iClient, iPort) in enumerate(zip(iconfig['settings']['clients'], clientPort)):
-                vmessConfig[i]['port'] = str(iPort)
-                vmessConfig[i]['id'] = iClient['id']
-                if iClient['alterId']:
-                    vmessConfig[i]['aid'] = str(iClient['alterId'])
-
-            for i in vmessConfig:
-                configOut.append(i)
-    return configOut
-
-if __name__ == '__main__':
-    data= urllib.request.urlopen('http://jsonip.com').read().decode()
-    publicIP = json.loads(data)['ip']
-
-    parser = argparse.ArgumentParser(description="""Script to generate v2rayN format configuration file based on
-    https://github.com/2dust/v2rayN/wiki/%E5%88%86%E4%BA%AB%E9%93%BE%E6%8E%A5%E6%A0%BC%E5%BC%8F%E8%AF%B4%E6%98%8E(ver-2)""")
-
-    parser.add_argument('-c', '--config', default='config.json', help='path of configuration file for v2ray server (default: %(default)s)')
-    parser.add_argument('-s', '--server', default=publicIP, help='domain or IP address of v2ray server (default: IP of current host %(default)s)')
-    parser.add_argument('-p', '--port', nargs='+', type=int, default=[80], help='ports that v2ray server listens to (default: the ones defined in configuration file)')
-    parser.add_argument('-o', '--output', default='STDOUT', help='name of v2rayN format configuration file (default: %(default)s)')
-    parser.add_argument('-a', '--append', action='store_true', help='append to output file (default: False)')
-
-    ARGS = parser.parse_args()
-    config = generate_vmess_config(ARGS, V2RAYN_TEMPLATE)
-    if ARGS.output == 'STDOUT':
-        print(json.dumps(config, indent=4, ensure_ascii=False))
+    if "streamSettings" in inbound:
+        sset = inbound["streamSettings"]
+    
+    if "network" in sset:
+        _net = sset["network"]
     else:
-        if ARGS.append:
-            if os.path.isfile(ARGS.output):
-                with open(ARGS.output, 'r') as f:
-                    configOld = json.load(f)
-                config = configOld + config
-        with open(ARGS.output, 'w', encoding='utf-8') as f:
-            json.dump(config, f, indent=4, ensure_ascii=False)
+        _net = "tcp"
+
+    if option.filter is not None:
+        for filt in option.filter:
+            if filt.startswith("!"):
+                if _net == filt[1:]:
+                    raise UnknowProtocolException()
+            else:
+                if _net != filt:
+                    raise UnknowProtocolException()
+    
+    if _net == "tcp":
+        if "tcpSettings" in sset and \
+            "header" in sset["tcpSettings"] and \
+            "type" in sset["tcpSettings"]["header"]:
+                _type = sset["tcpSettings"]["header"]["type"]
+
+        if "security" in sset:
+            _tls = sset["security"]
+
+    elif _net == "kcp":
+        if "kcpSettings" in sset and \
+            "header" in sset["kcpSettings"] and \
+            "type" in sset["kcpSettings"]["header"]:
+                _type = sset["kcpSettings"]["header"]["type"]
+
+    elif _net == "ws":
+        if "wsSettings" in sset and \
+            "headers" in sset["wsSettings"] and \
+            "Host" in sset["wsSettings"]["headers"]:
+                _host = sset["wsSettings"]["headers"]["Host"]
+
+        if "wsSettings" in sset and "path" in sset["wsSettings"]:
+                _path = sset["wsSettings"]["path"]
+
+        if "security" in sset:
+            _tls = sset["security"]
+
+    elif _net == "h2" or _net == "http":
+        if "httpSettings" in sset and \
+            "host" in sset["httpSettings"]:
+                _host = ",".join(sset["httpSettings"]["host"])
+        if "httpSettings" in sset and \
+            "path" in sset["httpSettings"]:
+                _path = sset["httpSettings"]["path"]
+        _tls = "tls"
+
+    elif _net == "quic":
+        if "quicSettings" in sset:
+            _host = sset["quicSettings"]["security"]
+            _path = sset["quicSettings"]["key"]
+            _type = sset["quicSettings"]["header"]["type"]
+
+    else:
+        raise UnknowProtocolException()
+
+    if "settings" in inbound and "clients" in inbound["settings"]:
+        for c in inbound["settings"]["clients"]:
+            vobj = dict(
+                id=c["id"], aid=str(c["alterId"]),
+                v="2", tls=_tls, add=_add, port=_port, type=_type, net=_net, path=_path, host=_host, ps="{}/{}".format(_add, _net))
+
+            # plain replace
+            for key, plain in plain_amends.items():
+                val = vobj.get(key, None)
+                if val is None:
+                    continue
+                vobj[key] = plain
+
+            # sed-like cmd replace
+            for key, opt in sed_amends.items():
+                val = vobj.get(key, None)
+                if val is None:
+                    continue
+                vobj[key] = re.sub(opt[0], opt[1], val, opt[2])
+                
+            vmessobjs.append(vobj)
+    return vmessobjs
+
+def parse_amendsed(val):
+    if not val.startswith("s"):
+        raise ValueError("not sed")
+    spliter = val[1:2]
+    _, pattern, repl, tags = sedcmd.split(spliter, maxsplit=4)
+    return pattern, repl, tags
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="json2vmess convert server side json into vmess links")
+    parser.add_argument('-a', '--addr',
+                        action="store",
+                        default="",
+                        help="server address. If not specified, program will detect the current IP")
+    parser.add_argument('-f', '--filter',
+                        action="append",
+                        help="Protocol Filter, useful for inbounds with different protocols. "
+                        "FILTER starts with ! means negative selection. Multiple filter is accepted.")
+    parser.add_argument('-m', '--amend',
+                        action="append",
+                        help="Amend to the output values, can be use multiple times. eg: -a port:80 -a ps:amended")
+    parser.add_argument('--debug',
+                        action="store_true",
+                        default=False,
+                        help="debug mode, show vmess obj")
+    parser.add_argument('json',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin,
+                        help="parse the server side json")
+
+    option = parser.parse_args()
+
+    host_ip = option.addr
+    if host_ip == "":
+        host_ip = get_host_ip()
+
+    sed_amends = {}
+    plain_amends = {}
+    if option.amend:
+        for s in option.amend:
+            key, sedcmd = s.split(":", maxsplit=1)
+            try:
+                pattern, repl, tags = parse_amendsed(sedcmd)
+            except ValueError:
+                plain_amends[key] = sedcmd
+                continue
+
+            reflag = 0
+            if "i" in tags:
+                reflag |= re.IGNORECASE
+            sed_amends[key] = [pattern, repl, reflag]
+
+    jsonobj = json.load(option.json)
+    parse_inbounds(jsonobj)
